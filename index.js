ba978696a91690f97040f7220fdbdd2a5ee4be52
steve-the-hermit <stevemacharia309@gmail.com>
2023-07-08 11:16:14 +0300

    assignment finally done

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+// Variable Declarations
+const num1 = 31;
+const num2 = 2;
+const num3 = 20;
+const num4 = 8;
+const numbers = [10, 5, 20, 3];
 
-// Write your code here
+// Multiply variable
+const multiply = num1 * num2;
+
+// Random variable
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+// Mod variable
+const mod = num3 % num4;
+
+// Max variable
+const max = Math.max(...numbers);
+
+// Export the variables
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max,
+};
